1850132488111a843fd98a822a7490969a9003f8
ArthurT <arthur.talpaert@isen-lille.fr>
2016-04-15 11:22:21 +0200

    ajout de source

---
+++ src.js
@@ -0,0 +1,20 @@
+window.onload = function()
+{
+    var canvas = document.getElementById('mon_canvas');
+        if(!canvas)
+        {
+            alert("Impossible de récupérer le canvas");
+            return;
+        }
+
+    var context = canvas.getContext('2d');
+        if(!context)
+        {
+            alert("Impossible de récupérer le context du canvas");
+            return;
+        }
+
+        /**
+         * Ici tu coderas
+         */
+}
